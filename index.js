b0f8196c865307f18b984b0334ea20033ed33f89
Guled <guletkadar@gmail.com>
2023-03-21 23:15:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+countDown(10);
+
+ function writeCards(arrNames, eventName) {
+    let thankMessage = []
+    for (let i = 0; i < arrNames.length; i++) {
+  
+      const message = `Thank you, ${arrNames[i]}, for the wonderful ${eventName} gift!`;
+       thankMessage.push(message);
+    }   
+    return thankMessage;
+  }
+ 
+
+  const arrNames = ["Guadalupe", "Ollie", "Aki"];
+  const eventName = "Surprise";
+  const output = writeCards(arrNames, eventName);
+console.log(output);
